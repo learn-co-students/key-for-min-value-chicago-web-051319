b508895da69affdf330ba99d58be65fa677b76aa
Tristan Jackson <tristan@audreysmonsters.com>
2019-04-02 00:36:12 -0500

    double pipe for hte win

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    lowest_key_store = nil
+    lowest_value_store = nil
+    name_hash.each do |key, value|
+        if lowest_value_store == nil || value < lowest_value_store
+            lowest_value_store = value
+            lowest_key_store = key
+        end
+    end
+    lowest_key_store
 end
+
+# compare the first two values, see which one is smaller, then compare to the next one and see which is smaller
